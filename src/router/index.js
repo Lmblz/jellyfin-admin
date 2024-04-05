d6ad98a998322745088d693e67d67b65685c1b88
Thomas LAMBOLEZ <lambolez.th@gmail.com>
2024-04-05 20:34:12 +0200

    Hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomePage from "../views/HomePage.vue";
 const Library = () => import("../views/LibraryView.vue");
@@ -55,7 +55,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 })
 
